a2866d20ab13df7e320a8810ccdc937d175f806f
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-11-20 06:50:22 +0800

    install package html loader css loader and styles loader and update it into our webpack config file
    to load html and css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-// webpack.config.js
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,4 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
