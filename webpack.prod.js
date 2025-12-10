c962685dd322c6e080327d2a9fac8e37743cdb4a
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-12-10 15:12:01 +0800

    fix: add publicpath for github pages deployment

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,4 +5,7 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  output: {
+    publicPath: '/Ariff_dev/', // Add this for GitHub Pages
+  },
 });
